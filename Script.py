c1066e4748143b89a550a495a9f69291b30ae967
ftmbotzx <kumarasta91@gmail.com>
2025-02-06 18:16:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,22 +1,35 @@
 #start, help $ other msgers..
 
 class script(object):
-  START_TXT = """
-  𝙷𝚎𝚕𝚕𝚘 {}, 𝙸'𝚊𝚖 <a href='https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template'>Pyrogram Bot</a>
-  𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙱𝚘𝚝.  
+  START_TXT = """🚩 जय श्री राम
+  
+  𝙷𝚎𝚕𝚕𝚘 {}, ɪ ᴀᴍ ᴠᴇʀʏ ᴀɴᴅ ᴍᴏsᴛ ᴘᴏᴡᴇʀꜰᴜʟ 🎥 YᴏᴜTᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ʙᴏᴛ ⚡ ᴛɪʟʟ ɴᴏᴡ ᴄʀᴇᴀᴛᴇᴅ ʙʏ Fᴛᴍ Dᴇᴠᴇʟᴏᴘᴇʀᴢ 🚀
+🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href='https://github.com/ftmdeveloperz'>Fᴛᴍ Dᴇᴠᴇʟᴏᴘᴇʀᴢ</a>
+
   """
   
   HELP_TXT = """
-  add help message for users
+  ℹ️ How to Use This Bot
+1️⃣ Send a YouTube video link.
+2️⃣ Choose the quality from the inline buttons.
+3️⃣ Wait while the bot processes your request.
+4️⃣ Receive the video or audio file.
+
+Supported Formats:
+- 🎥 MP4 (144p, 240p, 360p, 480p, 720p)
+- 🎞 MKV (1080p)
+- 🎵 MP3 (Audio)
+
+📩 Need help? Contact:  <a href='https://github.com/ftmdeveloperz'>Fᴛᴍ Dᴇᴠᴇʟᴏᴘᴇʀᴢ</a>
   """
   
   ABOUT_TXT = """
-✯ 𝙼𝚢 𝙽𝚊𝚖𝚎 : <a href='https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template'>Pyrogram Bot</a>
-✯ 𝙻𝚒𝚋𝚛𝚊𝚛𝚢 : <a href='https://docs.pyrogram.org/'>𝙿𝚢𝚛𝚘𝚐𝚛𝚊𝚖 v𝟷.𝟸.𝟸𝟶</a>
+✯ 𝙼𝚢 𝙽𝚊𝚖𝚎 : <a href='https://t.me/@ftmbotzx_ytdownloaderbot'>Fᴛᴍ TᴜʙᴇFᴇᴛᴄʜ</a>
+✯ 𝙻𝚒𝚋𝚛𝚊𝚛𝚢 : <a href='https://docs.pyrogram.org/'>𝙿𝚢𝚛𝚘𝚐𝚛𝚊𝚖 v2.𝟸.𝟸𝟶</a>
 ✯ 𝚂𝚎𝚛𝚟𝚎𝚛 : <a href='https://dashboard.heroku.com/'>𝙷𝚎𝚛𝚘𝚔𝚞</a>
-✯ 𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎 : <a href='https://docs.python.org/3/'>𝙿𝚢𝚝𝚑𝚘𝚗 𝟹.𝟿.𝟿</a>
+✯ 𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎 : <a href='https://docs.python.org/3/'>𝙿𝚢𝚝𝚑𝚘𝚗 𝟹.10.𝟿</a>
 ✯ 𝙳𝚊𝚝𝚊𝙱𝚊𝚜𝚎 : <a href='https://mongodb.com/'>𝙼𝚘𝚗𝚐𝚘𝙳𝙱</a>
-✯ 𝙱𝚊𝚜𝚎 𝚂𝚘𝚞𝚛𝚌𝚎 𝙲𝚘𝚍𝚎 : <a href='https://github.com/kalanakt/Pyrogram-Telegram-Bot-Template'>Pyrogram Telegram Bot Template</a>
-✯ 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 : <a href='https://t.me/TMWAD'>𝙲𝚕𝚒𝚌𝚔 𝙷𝚎𝚛𝚎</a>
-✯ 𝙼𝚊𝚒𝚗𝚝𝚎𝚗𝚊𝚗𝚌𝚎 : <a href='https://github.com/kalanakt'>𝙷𝚊𝚜𝚑𝙼𝚒𝚗𝚗𝚎𝚛</a>
+✯ 𝙱𝚊𝚜𝚎 𝚂𝚘𝚞𝚛𝚌𝚎 𝙲𝚘𝚍𝚎 : <a href='https://github.com/ftmbotzx/YouTube-Downloader'Fᴛᴍ TᴜʙᴇFᴇᴛᴄʜ></a>
+✯ 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 : <a href='https://t.me/ftmbotzx'>𝙲𝚕𝚒𝚌𝚔 𝙷𝚎𝚛𝚎</a>
+✯ 𝙼𝚊𝚒𝚗𝚝𝚎𝚗𝚊𝚗𝚌𝚎 : <a href='https://github.com/ftmdeveloperz'>Fᴛᴍ Dᴇᴠᴇʟᴏᴘᴇʀᴢ</a>
 """
